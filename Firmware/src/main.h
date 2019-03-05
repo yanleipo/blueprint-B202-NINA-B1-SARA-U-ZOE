bb4d8ee5af6d5ac2b4a8aab66cfbc2a9b63f7926
Leipo Yan <leipo.yan@u-blox.com>
2019-03-05 16:01:58 +0800

    Add missing main.h

---
+++ Firmware/src/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+void B202_NUS_LOG(char *format, ...);
+
+#define B202_LOG_ERROR(...) {B202_NUS_LOG(__VA_ARGS__); NRF_LOG_ERROR(__VA_ARGS__) ;}
+#define B202_LOG_WARNING(...) {B202_NUS_LOG(__VA_ARGS__); NRF_LOG_WARNING(__VA_ARGS__) ;}
+#define B202_LOG_INFO(...) {B202_NUS_LOG(__VA_ARGS__); NRF_LOG_INFO(__VA_ARGS__) ;}
+#define B202_LOG_DEBUG(...) {B202_NUS_LOG(__VA_ARGS__); NRF_LOG_DEBUG(__VA_ARGS__) ;}
+
+#endif
